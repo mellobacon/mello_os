cd1321891ef4a7efb08488cef79dafb1493e5221
mellobacon <mellodev@outlook.com>
2024-02-25 16:36:49 -0600

    nodepolyfills
    Signed-off-by: mellobacon <mellodev@outlook.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), nodePolyfills({
+    globals: {
+      Buffer: true,
+      process: true,
+    },
+    overrides: {
+      fs: "memfs"
+    }
+  }),],
 })
