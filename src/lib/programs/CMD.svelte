34b882e4260e64b2633c60f3837ece5e9b2cb664
mellobacon <mellodev@outlook.com>
2024-02-25 16:37:56 -0600

    moved terminal handling to dedicated file
    Signed-off-by: mellobacon <mellodev@outlook.com>

--- src/lib/programs/CMD.svelte
+++ src/lib/programs/CMD.svelte
@@ -2,10 +2,8 @@
     import { onMount } from "svelte";
     import Container from "../Container.svelte";
     import Program from "../Program.svelte";
-    import { Terminal } from "xterm";
-    import { FitAddon } from "xterm-addon-fit";
-    import { CommandHandler } from "../../scripts/cmd";
     import "xterm/css/xterm.css";
+    import { CMD } from "../../scripts/cmd";
 
     export let index;
     export let name;
@@ -17,113 +15,6 @@
         termInstance = new CMD(terminal);
         termInstance.initTerm();
     });
-</script>
-
-<script lang="ts" context="module">
-    let prompt = "C:\>";
-
-    var baseTheme = {
-        foreground: "#F8F8F8",
-        background: "#000",
-        selection: "#5DA5D533",
-        black: "#1E1E1D",
-        brightBlack: "#262625",
-        red: "#CE5C5C",
-        brightRed: "#FF7272",
-        green: "#5BCC5B",
-        brightGreen: "#72FF72",
-        yellow: "#CCCC5B",
-        brightYellow: "#FFFF72",
-        blue: "#5D5DD3",
-        brightBlue: "#7279FF",
-        magenta: "#BC5ED1",
-        brightMagenta: "#E572FF",
-        cyan: "#5DA5D5",
-        brightCyan: "#72F0FF",
-        white: "#F8F8F8",
-        brightWhite: "#FFFFFF",
-    };
-    var customTheme = {
-        foreground: "#eff0eb",
-        background: "#282a36",
-        selection: "#97979b33",
-        black: "#282a36",
-        brightBlack: "#686868",
-        red: "#ff5c57",
-        brightRed: "#ff5c57",
-        green: "#5af78e",
-        brightGreen: "#5af78e",
-        yellow: "#f3f99d",
-        brightYellow: "#f3f99d",
-        blue: "#57c7ff",
-        brightBlue: "#57c7ff",
-        magenta: "#ff6ac1",
-        brightMagenta: "#ff6ac1",
-        cyan: "#9aedfe",
-        brightCyan: "#9aedfe",
-        white: "#f1f1f0",
-        brightWhite: "#eff0eb",
-    };
-
-    class CMD {
-        private term = new Terminal({
-            allowProposedApi: true,
-            cursorBlink: true,
-            fontFamily: "VT323",
-            theme: baseTheme
-        });
-        private termFit = new FitAddon();
-        private commands: CommandHandler;
-        constructor(terminal: HTMLElement) {
-            this.termFit = new FitAddon();
-            this.term.loadAddon(this.termFit);
-            this.term.open(terminal);
-            this.termFit.fit();
-            this.term.focus();
-
-            let obs = new ResizeObserver((e) => {
-                this.termFit.fit();
-            })
-            obs.observe(terminal)
-
-            this.commands = new CommandHandler(this.term);
-        }
-        initTerm() {
-            this.term.writeln("Mellosoft(R) Mello OS")
-            this.term.writeln("Copyright (C) Mellosoft Inc. All rights nonexistent.")
-            this.prompt();
-
-            this.term.onData(async (e) => {
-                switch (e) {
-                    case "\u0003": // Ctrl+C:
-                        this.term.write("^C");
-                        this.prompt();
-                        break;
-                    case "\r": // Enter
-                        this.term.writeln("");
-                        await this.commands.executeCommand();
-                        this.prompt();
-                        break;
-                    case "\u007F": // Backspace (DEL)
-                        if (this.term.buffer.normal.cursorX > 4) {
-                            this.term.write("\b \b");
-                            this.commands.removeCommandChar();
-                        }
-                        break;
-                    default:
-                        if ((e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7e)) ||
-                            e >= "\u00a0") {
-                            this.term.write(e);
-                            this.commands.addCommandChar(e);
-                        }
-                        break;
-                }
-            });
-        }
-        private prompt() {
-            this.term.write(`\r\n${prompt} `);
-        }
-    }
 </script>
 
 <Program {name} {index}>
--- src/scripts/cmd.ts
+++ src/scripts/cmd.ts
@@ -1,86 +1,110 @@
-import type { Terminal } from "xterm";
-import * as cmd from "./commands";
+import { Terminal } from "xterm";
+import { FitAddon } from "xterm-addon-fit";
+import { CommandHandler } from "./command_handler";
+import { getHome } from "./filesystem";
+var baseTheme = {
+    foreground: "#F8F8F8",
+    background: "#000",
+    selection: "#5DA5D533",
+    black: "#1E1E1D",
+    brightBlack: "#262625",
+    red: "#CE5C5C",
+    brightRed: "#FF7272",
+    green: "#5BCC5B",
+    brightGreen: "#72FF72",
+    yellow: "#CCCC5B",
+    brightYellow: "#FFFF72",
+    blue: "#5D5DD3",
+    brightBlue: "#7279FF",
+    magenta: "#BC5ED1",
+    brightMagenta: "#E572FF",
+    cyan: "#5DA5D5",
+    brightCyan: "#72F0FF",
+    white: "#F8F8F8",
+    brightWhite: "#FFFFFF",
+};
+var customTheme = {
+    foreground: "#eff0eb",
+    background: "#282a36",
+    selection: "#97979b33",
+    black: "#282a36",
+    brightBlack: "#686868",
+    red: "#ff5c57",
+    brightRed: "#ff5c57",
+    green: "#5af78e",
+    brightGreen: "#5af78e",
+    yellow: "#f3f99d",
+    brightYellow: "#f3f99d",
+    blue: "#57c7ff",
+    brightBlue: "#57c7ff",
+    magenta: "#ff6ac1",
+    brightMagenta: "#ff6ac1",
+    cyan: "#9aedfe",
+    brightCyan: "#9aedfe",
+    white: "#f1f1f0",
+    brightWhite: "#eff0eb",
+};
+export class CMD {
+    private term = new Terminal({
+        allowProposedApi: true,
+        cursorBlink: true,
+        fontFamily: "VT323",
+        theme: baseTheme
+    });
+    private termFit = new FitAddon();
+    private commands: CommandHandler;
+    current_dir = getHome();
+    private _prompt = `C:${this.current_dir}>`;
+    constructor(terminal: HTMLElement) {
+        this.termFit = new FitAddon();
+        this.term.loadAddon(this.termFit);
+        this.term.open(terminal);
+        this.termFit.fit();
+        this.term.focus();
 
-export class CommandHandler {
-    private term: Terminal;
-    private command = "";
-    private commands = {}
-    constructor(term: Terminal) {
-        this.term = term;
-        this.commands = {
-            help: {
-                description: "Prints all commands",
-                syntax: "help [command]",
-                args: ["command"],
-                argsd: "command - display help info on that command",
-                function: (term) => cmd.help(term, this.commands)
-            },
-            cls: {
-                description: "Clear Terminal",
-                syntax: "cls",
-                function: (term) => {cmd.clear(term)}
-            },
-            echo: {
-                description: "Displays messages",
-                syntax: "echo [message]",
-                args: ["message"],
-                argsd: "message - text to display",
-                function: (term, args) => {cmd.echo(term, args)}
-            },
-            exit: {
-                description: "Quits CMD program",
-                syntax: "exit",
-                function: (term) => {cmd.exit(term)}
-            },
-            access: {
-                description: "Attempt to access a security system",
-                syntax: "access [message]",
-                argsd: "message - thing to access",
-                function: async (term) => {await cmd.access(term)}
-            }
-        }
-    }
-    addCommandChar(c: string) {
-        this.command += c;
-    }
-    async executeCommand() {
-        let command = this.command.trim();
-        if (command.length === 0) {
-            return;
-        }
-        let args = command.split(" ");
-        let arg0 = args.shift() ?? "";
-        this.command = arg0;
-        
-        if (!(this.command in this.commands)) {
-            this.term.writeln(`${this.command}: command not found`);
-            this.command = "";
-            return;
-        }
+        let obs = new ResizeObserver((e) => {
+            this.termFit.fit();
+        })
+        obs.observe(terminal)
 
-        if (args[0] === "/?") {
-            this.fetchHelp()
-        }
-        else {
-            await this.commands[this.command].function(this.term, args);
-        }
-        this.command = "";
+        this.commands = new CommandHandler(this.term, this);
     }
-    removeCommandChar() {
-        if (this.commandLength() > 0) {
-            this.command = this.command.substring(0, this.command.length - 1);
-        }
-    }
-    commandLength() {
-        return this.command.length;
-    }
-    fetchHelp() {
-        if (this.commands[this.command]) {
-            this.term.writeln(`${this.commands[this.command].description}\r\n`);
-            this.term.writeln(`syntax: ${this.commands[this.command].syntax}`);
-            if (this.commands[this.command].argsd) {
-                this.term.writeln(`\t${this.commands[this.command].argsd}`);
+    initTerm() {
+        this.term.writeln("Mellosoft(R) Mello OS")
+        this.term.writeln("Copyright (C) Mellosoft Inc. All rights nonexistent.")
+        this.prompt();
+
+        this.term.onData(async (e) => {
+            switch (e) {
+                case "\u0003": // Ctrl+C:
+                    this.term.write("^C");
+                    this.prompt();
+                    break;
+                case "\r": // Enter
+                    this.term.writeln("");
+                    await this.commands.executeCommand();
+                    this.prompt();
+                    break;
+                case "\u007F": // Backspace (DEL)
+                    if (this.term.buffer.normal.cursorX > this._prompt.length + 1) {
+                        this.term.write("\b \b");
+                        this.commands.removeCommandChar();
+                    }
+                    break;
+                default:
+                    if ((e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7e)) ||
+                        e >= "\u00a0") {
+                        this.term.write(e);
+                        this.commands.addCommandChar(e);
+                    }
+                    break;
             }
-        }
+        });
+    }
+    private prompt() {
+        this.term.write(`\r\n${this._prompt} `);
+    }
+    setPrompt(input: string) {
+        this._prompt = `C:${input}>`
     }
 }
