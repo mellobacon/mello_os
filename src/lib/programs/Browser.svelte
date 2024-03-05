848cf422a16c9a2a71f37b62990ddbf2e84b675b
mellobacon <mellodev@outlook.com>
2024-03-04 23:06:29 -0600

    created function browser
    Signed-off-by: mellobacon <mellodev@outlook.com>

--- src/lib/programs/Browser.svelte
+++ src/lib/programs/Browser.svelte
@@ -1,6 +1,8 @@
-<script>
+<script lang="ts">
+    import Button from "../Button.svelte";
     import Container from "../Container.svelte";
-import Input from "../Input.svelte";
+    import Dialog from "../Dialog.svelte";
+    import Input from "../Input.svelte";
     import Program from "../Program.svelte";
     import Toolbar from "../Toolbar.svelte";
 
@@ -9,26 +11,85 @@ import Input from "../Input.svelte";
 
     let tools = [
         {name: "File", items: [
-            {name: "New Window", action: () => {console.log("e")}},
-            {name: "Quit", action: () => {console.log("e")}}
+            {name: "About", action: showAbout},
+            {name: "Quit", action: quitWindow}
         ]},
-        {name: "Edit", items: [
-            {name: "Undo", action: () => {console.log("e")}},
-            {name: "Redo", action: () => {console.log("e")}}
-        ]}
     ]
+
+    let ref;
+    function quitWindow() {
+        ref.$destroy()
+    }
+    function showAbout() {
+        new Dialog({target: document.getElementById("icons")!, props: {
+            "type": "alert", 
+            "action_type": "info", 
+            "title": "About browser", 
+            message: "Welcome to Browser. This browser is able to load websites from the 90s"
+        }})
+    }
+
+    let url = "";
+    let src = "";
+    let urls = [
+        "http://info.cern.ch/hypertext/WWW/TheProject.html"
+    ];
+    async function processURL() {
+        let request = await fetch(new Request(`https://archive.org/wayback/available?url=${url}&timestamp=1990`))
+        name = "Browser (loading url...)"
+        if (request.status === 200) {
+            let json = await request.json();
+            if (!json.archived_snapshots.closest) {
+                name = "Browser (url error)"
+                new Dialog({target: document.getElementById("icons")!, props: {
+                    "type": "alert", 
+                    "action_type": "error", 
+                    "title": "URL Not Found", 
+                    message: "ERROR: URL cannot be found or does not exist"
+                }})
+                return;
+            }
+            let timestamp: string = json.archived_snapshots.closest.timestamp.slice(0, 4);
+            if (parseInt(timestamp) > 1999) {
+                name = "Browser (url error)"
+                new Dialog({target: document.getElementById("icons")!, props: {
+                    "type": "alert", 
+                    "action_type": "error", 
+                    "title": "URL Not Found", 
+                    message: "ERROR: URL cannot be found or does not exist"
+                }})
+            }
+            else {
+                src = json.archived_snapshots.closest.url;
+                if (!urls.includes(url)) {
+                    urls = [...urls, url];
+                }
+                name = `Browser (${url})`
+            }
+        }
+        else {
+            new Dialog({target: document.getElementById("icons")!, props: {
+                "type": "alert", 
+                "action_type": "error", 
+                "title": "Internal Error", 
+                message: "ERROR: Cannot process URL"
+            }})
+            name = "Browser (internal error)"
+        }
+    }
 </script>
 
-<Program {name} {index}>
+<Program {name} {index} bind:this={ref}>
     <Toolbar slot="toolbar" {tools}>
         <div class="addressbar">
             <div>Address: </div>
-            <Input nolabel type="url" width="100%" />
+            <Input nolabel type="url" bind:input={url} width="100%" dropdown dropdown_items={urls} />
+            <Button label="GO" width="24px" height="24px" on:click={processURL} />
         </div>
     </Toolbar>
     <div class="browser">
-        <Container background="white" height="100%" _class="browser-container">
-            <iframe title="web" src="http://info.cern.ch/hypertext/WWW/TheProject.html"></iframe>
+        <Container background="transparent" height="100%" _class="browser-container">
+            <iframe title="" src={src}></iframe>
         </Container>
     </div>
 </Program>
@@ -42,6 +103,9 @@ import Input from "../Input.svelte";
         gap: 5px;
         border-top: 1.5px solid #fcfcfc;
         padding: 3px;
+    }
+    :global(.addressbar button) {
+        margin: 0 !important;
     }
     iframe {
         width: 100%;
